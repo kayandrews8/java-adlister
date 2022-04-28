e7997eead5f7586839e48873491e0ef8d2a24e2d
Kaylah Andrews <kandr716@gmail.com>
2022-04-28 11:10:53 -0500

    creat beans and bean test

---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,5 @@
+public class BeanTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Quote.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+
+public class Quote implements Serializable {
+    private String content;
+    private String author;
+
+    public Quote(){}
+
+    public Quote(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
